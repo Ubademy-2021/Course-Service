e65531f424eddd6bb757062a890686be02466351
Martin <martincohen98@gmail.com>
2021-11-06 00:43:19 -0300

    More generic error text

--- app/adapters/http/util/userServiceUtil.py
+++ app/adapters/http/util/userServiceUtil.py
@@ -22,5 +22,5 @@ class UserServiceUtil:
     def check_user_exists(id):
         logger.info("Checking if user exists in user service")
         if not UserServiceUtil.makeUserRequest(id):
-            logger.error("User can not be add as a colaborator because it does not exist")
+            logger.error("User does not exist")
             raise HTTPException(status_code=400, detail="User does not exist")
